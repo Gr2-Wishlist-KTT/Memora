322433592a551ad4bd32b80572c29c5de9c5ad8c
kasper <kasperj2016@gmail.com>
2026-04-13 09:32:06 +0200

    user

---
+++ src/main/java/com/example/memora/model/User.java
@@ -0,0 +1,16 @@
+package com.example.memora.model;
+
+import java.util.List;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+
+    public User (int id, String name, String email){
+        this.id = id;
+    }
+
+
+
+}
